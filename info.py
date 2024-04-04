38ee38a0a5dfab0139070046efb1dfe0e799c579
sivanth07 <152179608+sivanth07@users.noreply.github.com>
2024-04-04 19:07:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://jack-reacher-bmdp.onrender.com/".format(FQDN)
+    URL = "https://sivathor-bec5813b17b9.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://jack-reacher-bmdp.onrender.com/".format(FQDN)
+    URL = "https://sivathor-bec5813b17b9.herokuapp.com/".format(FQDN)
 
 
 
