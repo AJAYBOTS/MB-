5e8dce3d230178393e9e8ffd8fa61e3583beccd8
sivanth07 <152179608+sivanth07@users.noreply.github.com>
2024-04-04 19:48:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://svnh-auto-filter-cb378710a571.herokuapp.com/".format(FQDN)
+    URL = "https://jack-reacher-bmdp.onrender.com/".format(FQDN)
 else:
-    URL = "https://svnh-auto-filter-cb378710a571.herokuapp.com/".format(FQDN)
+    URL = "https://jack-reacher-bmdp.onrender.com/".format(FQDN)
 
 
 
